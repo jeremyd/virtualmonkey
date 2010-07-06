65b81cd2fdbd42929a00a42e9dd0c058739b0397
Jeremy Deininger <jeremy@rightscale.com>
2010-07-06 10:57:44 -0700

    adding to spec for bug

--- spec/bug3518.rb
+++ spec/bug3518.rb
@@ -4,6 +4,8 @@ describe "bug #3518" do
   before(:all) do
     raise "for this spec to run, you must have a mysql deployment nickname in $DEPLOYMENT variable" unless ENV['DEPLOYMENT']
     @runner = VirtualMonkey::MysqlRunner.new(ENV['DEPLOYMENT'])
+    @runner.lookup_scripts
+    @runner.setup_dns
   end
   it "disables binlogs on the old master after promote" do
     @runner.run_promotion_operations
