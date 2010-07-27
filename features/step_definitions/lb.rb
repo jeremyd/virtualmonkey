e1c67161329eaacfd7e550786cf5f631287cc31b
Jeremy Deininger <jeremy@rightscale.com>
2010-07-26 17:38:06 -0700

    fixing up the fe/app features, wip

--- features/step_definitions/lb.rb
+++ features/step_definitions/lb.rb
@@ -1,5 +1,13 @@
 require File.expand_path(File.join(File.dirname(__FILE__) , '..','..','spec','spec_helper'))
 
+Then /^I should run log rotation checks/ do
+  @runner.log_rotation_checks
+end
+
+Then /^I should run frontend checks/ do
+  @runner.frontend_checks
+end
+
 Then /^I should cross connect the frontends/ do
   @runner.cross_connect_frontends
 end
