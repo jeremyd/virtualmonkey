a0d3ca3f4e28dad583b4b959d49c4b54f4030c55
Jeremy Deininger <jeremy@rightscale.com>
2010-08-05 09:36:20 -0700

    fixing fe_checks for chef, fixing inputs for unified application

--- lib/virtualmonkey/php_chef_runner.rb
+++ lib/virtualmonkey/php_chef_runner.rb
@@ -2,5 +2,52 @@ module VirtualMonkey
   class PhpChefRunner
     include VirtualMonkey::DeploymentRunner
     include VirtualMonkey::ApplicationFrontend
+
+    def frontend_checks
+      detect_os
+
+      run_unified_application_checks(fe_servers, 80)
+
+      # check that all application servers exist in the haproxy config file on all fe_servers
+      server_ips = Array.new
+      app_servers.each { |app| server_ips << app['private-ip-address'] }
+      fe_servers.each do |fe|
+        fe.settings
+        haproxy_config = fe.spot_check_command('cat /home/haproxy/rightscale_lb.cfg | grep server')
+        server_ips.each { |ip|  haproxy_config.to_s.include?(ip).should == true }
+      end
+
+      # restart haproxy and check that it succeeds
+      fe_servers.each_with_index do |server,i|
+        response = server.spot_check_command?('service haproxy stop')
+        raise "Haproxy stop command failed" unless response
+
+        stopped = false
+        count = 0
+        until response || count > 3 do
+          response = server.spot_check_command(server.haproxy_check)
+          stopped = response.include?("not running")
+          break if stopped
+          count += 1
+          sleep 10
+        end
+
+        response = server.spot_check_command?('service haproxy start')
+        raise "Haproxy start failed" unless response
+      end
+    end
+
+    def lookup_scripts
+      @scripts_to_run = {}
+      st = ServerTemplate.find(fe_servers.first.server_template_href)
+      @scripts_to_run["attach"] = st.executables.detect { |d| d.recipe == "lb_haproxy::do_attach_request" }
+    end
+
+    def cross_connect_frontends
+      statuses = Array.new 
+      @servers.each { |s| statuses << s.run_executable(@scripts_to_run['attach']) }
+      statuses.each_with_index { |s,i| s.wait_for_completed }
+    end
+    
   end
 end
