9ec5f906f98e8f3db1e3be5e29ad1e4167efb8ff
Jeremy Deininger <jeremy@rightscale.com>
2010-09-17 14:17:13 -0700

    temporary removing required files to support config file option

--- lib/virtualmonkey/command/create.rb
+++ lib/virtualmonkey/command/create.rb
@@ -4,10 +4,10 @@ module VirtualMonkey
 # monkey create --server_template_ids 123,123 --common_inputs blah.json --feature simple.feature --tag unique_name --TBD:filter?
     def self.create
       options = Trollop::options do
-        opt :server_template_ids, "ServerTemplate ids to use for creating the deployment.  Use one ID per server that you would like to be in the deployment.  Accepts space separated integers, or one argument per id. Eg. -s 23747 23747", :type => :integers, :required => true, :short => '-s'
-        opt :common_inputs, "Paths to common input json files to load and set on all deployments.  Accepts space separated pathnames or one argument per pathname.  Eg. -c config/mysql_inputs.json -c config/other_inputs.json", :type => :strings, :required => true, :short => '-c'
-        opt :tag, "Tag to use as nickname prefix for all deployments.", :type => :string, :required => true, :short => '-t'
-        opt :cloud_variables, "Path to json file containing common inputs and variables per cloud. See config/cloud_variables.json.example", :type => :string, :required => true, :short => '-v'
+        opt :server_template_ids, "ServerTemplate ids to use for creating the deployment.  Use one ID per server that you would like to be in the deployment.  Accepts space separated integers, or one argument per id. Eg. -s 23747 23747", :type => :integers, :required => false, :short => '-s'
+        opt :common_inputs, "Paths to common input json files to load and set on all deployments.  Accepts space separated pathnames or one argument per pathname.  Eg. -c config/mysql_inputs.json -c config/other_inputs.json", :type => :strings, :required => false, :short => '-c'
+        opt :tag, "Tag to use as nickname prefix for all deployments.", :type => :string, :required => false, :short => '-t'
+        opt :cloud_variables, "Path to json file containing common inputs and variables per cloud. See config/cloud_variables.json.example", :type => :string, :required => false, :short => '-v'
         opt :file, "Read configuration from this troop file (optional)", :type => :string, :required => true
         opt :no_spot, "Do not use spot instances"
       end
