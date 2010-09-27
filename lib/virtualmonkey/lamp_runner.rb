af734c5bdbeccb88aca42827eb27ff40f525728c
Jeremy Deininger <jeremy@rightscale.com>
2010-09-27 11:34:07 -0700

    lib/virtualmonkey/lamp_runner.rb

--- lib/virtualmonkey/lamp_runner.rb
+++ lib/virtualmonkey/lamp_runner.rb
@@ -4,7 +4,17 @@ module VirtualMonkey
     include VirtualMonkey::UnifiedApplication
 
     def run_lamp_checks
+      # check that the standard unified app is responding on port 80
       run_unified_application_checks(@servers, 80)
+      
+      # TODO: check that running the mysql backup script succeeds
+      # server.spot_check_command("/etc/cron.daily/mysql-dump-backup.sh")
+      #
+      # TODO: run operational RightScript(s)
+      # 
+
+
+      
     end
   end
 end
