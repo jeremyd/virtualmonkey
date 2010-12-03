ca48820e11da8097155becc2053cdce6e669d029
Tim Rodriguez <timothy@rightscale.com>
2010-12-03 14:36:05 -0800

    bugfix

--- features/base.rb
+++ features/base.rb
@@ -6,13 +6,13 @@
 # Scenario: base server test
 #
 # Given A simple deployment
-  runner = VirtualMonkey::SimpleRunner.new(ENV['DEPLOYMENT'])
+  @runner = VirtualMonkey::SimpleRunner.new(ENV['DEPLOYMENT'])
 
 # Then I should stop the servers
-  runner.behavior(:stop_all)
+  @runner.behavior(:stop_all)
 
 # Then I should launch all servers
-  runner.behavior(:launch_all)
+  @runner.behavior(:launch_all)
 
 # Then I should wait for the state of "all" servers to be "operational"
   @runner.behavior(:wait_for_all, "operational")
