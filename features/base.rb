de9d499171fa8c99fa466fe9de45aba5b497a9af
Tim Rodriguez <timothy@rightscale.com>
2010-12-03 14:31:07 -0800

    Usability

--- features/base.rb
+++ features/base.rb
@@ -1,32 +1,31 @@
 #@base
 #
-#Feature: Base Server Test
-#  Tests the base server functions
+# Feature: Base Server Test
+#   Tests the base server functions
 #
-#Scenario: base server test
+# Scenario: base server test
 #
-#  Given A simple deployment
-  raise "FATAL:  Please set the environment variable $DEPLOYMENT" unless ENV['DEPLOYMENT']
-  @runner = VirtualMonkey::SimpleRunner.new(ENV['DEPLOYMENT'])
-#  Then I should stop the servers
-  @runner.stop_all
+# Given A simple deployment
+  runner = VirtualMonkey::SimpleRunner.new(ENV['DEPLOYMENT'])
 
-#  Then I should launch all servers
-  @runner.launch_all
+# Then I should stop the servers
+  runner.behavior(:stop_all)
 
-#  Then I should wait for the state of "all" servers to be "operational"
-    state = "operational"
-    @runner.wait_for_all(state)
-#  Then I should check that monitoring is enabled
-  @runner.check_monitoring
+# Then I should launch all servers
+  runner.behavior(:launch_all)
 
-#  Then I should reboot the servers
-  @runner.reboot_all
+# Then I should wait for the state of "all" servers to be "operational"
+  @runner.behavior(:wait_for_all, "operational")
 
-#  Then I should wait for the state of "all" servers to be "operational"
-    state = "operational"
-    @runner.wait_for_all(state)
+# Then I should check that monitoring is enabled
+  @runner.behavior(:check_monitoring)
 
-#  Then I should check that monitoring is enabled
-  @runner.check_monitoring
+# Then I should reboot the servers
+  @runner.behavior(:reboot_all)
+
+# Then I should wait for the state of "all" servers to be "operational"
+  @runner.behavior(:wait_for_all, "operational")
+
+# Then I should check that monitoring is enabled
+  @runner.behavior(:check_monitoring)
 
