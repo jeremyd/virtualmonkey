b5d82c1dba44a835b3cf18d7ce985f6d7e51919b
Tim Rodriguez <timothy@rightscale.com>
2010-12-09 00:16:09 +0000

    updated to use table based lookup_scripts

--- lib/virtualmonkey/ebs_runner.rb
+++ lib/virtualmonkey/ebs_runner.rb
@@ -6,17 +6,29 @@ module VirtualMonkey
 
     # lookup all the RightScripts that we will want to run
     def lookup_scripts
+      scripts = [
+                 [ 'backup', 'EBS stripe volume backup' ],
+                 [ 'restore', 'EBS stripe volume restore' ],
+                 [ 'continuous_backup', 'EBS continuous backups' ],
+                 [ 'unfreeze', 'EBS unfreeze volume backups' ],
+                 [ 'freeze', 'EBS freeze volume backups' ],
+                 [ 'create_stripe', 'EBS stripe volume create' ],
+                 [ 'create_backup_scripts', 'EBS create backup scripts' ],
+                 [ 'grow_volume', 'EBS stripe volume grow and restore' ],
+                 [ 'terminate', 'TERMINATE' ]
+               ]
       st = ServerTemplate.find(s_one.server_template_href)
-      @scripts_to_run = {}
-      @scripts_to_run['backup'] = st.executables.detect { |ex| ex.name =~ /EBS stripe volume backup/ }
-      @scripts_to_run['restore'] = st.executables.detect { |ex| ex.name =~  /EBS stripe volume restore/i }
-      @scripts_to_run['continuous_backup'] = st.executables.detect { |ex| ex.name =~  /EBS continuous backups/i }
-      @scripts_to_run['unfreeze'] = st.executables.detect { |ex| ex.name =~  /EBS unfreeze volume backups/i }
-      @scripts_to_run['freeze'] = st.executables.detect { |ex| ex.name =~  /EBS freeze volume backups/i }
-      @scripts_to_run['create_stripe'] = st.executables.detect { |ex| ex.name =~  /EBS stripe volume create/i }
-      @scripts_to_run['create_backup_scripts'] = st.executables.detect { |ex| ex.name =~  /EBS create backup scripts/i }
-      @scripts_to_run['grow_volume'] = st.executables.detect { |ex| ex.name =~  /EBS stripe volume grow and restore/i }
-      @scripts_to_run['terminate'] = st.executables.detect { |ex| ex.name =~ /TERMINATE/ }
+      lookup_scripts_table(st,scripts)
+#      @scripts_to_run = {}
+#      @scripts_to_run['backup'] = st.executables.detect { |ex| ex.name =~ /EBS stripe volume backup/ }
+#      @scripts_to_run['restore'] = st.executables.detect { |ex| ex.name =~  /EBS stripe volume restore/i }
+#      @scripts_to_run['continuous_backup'] = st.executables.detect { |ex| ex.name =~  /EBS continuous backups/i }
+#      @scripts_to_run['unfreeze'] = st.executables.detect { |ex| ex.name =~  /EBS unfreeze volume backups/i }
+#      @scripts_to_run['freeze'] = st.executables.detect { |ex| ex.name =~  /EBS freeze volume backups/i }
+#      @scripts_to_run['create_stripe'] = st.executables.detect { |ex| ex.name =~  /EBS stripe volume create/i }
+#      @scripts_to_run['create_backup_scripts'] = st.executables.detect { |ex| ex.name =~  /EBS create backup scripts/i }
+#      @scripts_to_run['grow_volume'] = st.executables.detect { |ex| ex.name =~  /EBS stripe volume grow and restore/i }
+#      @scripts_to_run['terminate'] = st.executables.detect { |ex| ex.name =~ /TERMINATE/ }
     end
     
     # Create a stripe and write some data to it
