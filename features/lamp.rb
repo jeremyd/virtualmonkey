52886140d7f5ad0c11f206d0a7782ea204a895a8
Tim Rodriguez <timothy@rightscale.com>
2010-12-08 17:41:01 -0800

    more testcases

---
+++ features/lamp.rb
@@ -0,0 +1,33 @@
+#@lamp_test
+#
+#Feature: LAMP Server Template Test
+#  Tests the deployment
+#
+#Scenario: LAMP Server Template Test
+#
+# Given A LAMP deployment
+  @runner = VirtualMonkey::LampRunner.new(ENV['DEPLOYMENT'])
+
+# Then I should stop the servers
+  @runner.behavior(:stop_all)
+
+# Then I should launch all servers
+  @runner.behavior(:launch_all)
+
+# Then I should wait for the state of "all" servers to be "operational"
+  @runner.behavior(:wait_for_all, "operational")
+
+# Then I should run LAMP checks
+  @runner.behavior(:run_lamp_checks)
+
+# Then I should run mysql checks
+  @runner.behavior(:run_checks)
+
+## Then I should run mysqlslap stress test
+#  @runner.behavior(:run_mysqlslap_check)
+#
+# Then I should check that ulimit was set correctly
+  @runner.probe(".*", "su - mysql -s /bin/bash -c \"ulimit -n\"")
+
+# Then I should check that monitoring is enabled
+  @runner.behavior(:check_monitoring)
