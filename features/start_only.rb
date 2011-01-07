dc5d4e24d81ef42d6ca5d063e851d7068fe961d2
Cary <cary@rightscale.com>
2011-01-07 07:43:22 +0000

    added start_only feature

---
+++ features/start_only.rb
@@ -0,0 +1,26 @@
+#@base
+#
+# Feature: Base Server Test
+#   Tests the base server functions
+#
+# Scenario: base server test
+#
+# Given A simple deployment
+  @runner = VirtualMonkey::SimpleRunner.new(ENV['DEPLOYMENT'])
+
+# Then I should stop the servers
+  @runner.behavior(:stop_all)
+
+# Then I should launch all servers
+  @runner.behavior(:launch_all)
+
+# Then I should wait for the state of "all" servers to be "operational"
+  @runner.behavior(:wait_for_all, "operational")
+
+# Then I should reboot the servers
+  @runner.behavior(:reboot_all)
+
+# Then I should wait for the state of "all" servers to be "operational"
+  @runner.behavior(:wait_for_all, "operational")
+
+
